25449f014b42ee48fae415349aa40a3770ee4511
LadyByrdie <114427314+LadyByrdie@users.noreply.github.com>
2022-11-14 17:44:03 +0100

    la naissance de traitres

---
+++ src/personnages/Traitre.java
@@ -0,0 +1,10 @@
+package personnages;
+
+public class Traitre extends Samourai {
+    int niveauTraitrise = 0;
+
+    public Traitre(String seigneur, String nom, String boisson, int argent) {
+	super(seigneur, nom, boisson, argent);
+    }
+
+}
